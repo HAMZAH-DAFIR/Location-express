9fe970c57097a63ba645b093dff1757d4784df73
HAMZAH-DAFIR <80524090+HAMZAH-DAFIR@users.noreply.github.com>
2021-07-05 23:39:17 +0100

    connect to DB

--- index.js
+++ index.js
@@ -3,8 +3,17 @@ const app = express()
 // recuperer les variable d'envirenement
 require('dotenv').config();
 
+const mongoose = require('mongoose');
+// Connect to data base
+mongoose.connect(process.env.DATABASE, {
+    useCreateIndex : true,
+    useNewUrlParser : true,
+    useUnifiedTopology : true
+}).then(() => console.log('DATABASE is connect'))
+   .catch(() => console.log('Not connect to DATABASE !!')); 
+
 
 
 
 const port = process.env.PORT || 3000 ;
-app.listen(port,() => console.log(`The app listining the port http://localhost/${port}`));
+app.listen(port,() => console.log(`The app is running on port http://localhost/${port}`));
