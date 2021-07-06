8bc14ba5cb03a182c8bf4bde188a588c0ca76e6d
HAMZAH-DAFIR <80524090+HAMZAH-DAFIR@users.noreply.github.com>
2021-07-06 16:10:43 +0100

    fixe bugs

--- models/user.js
+++ models/user.js
@@ -41,27 +41,27 @@ const userScheme = new mongoose.Schema({
     
 },{timestamps:true})
 
-userScheme.virtual(password)
-          .set((password) => {
+userScheme.virtual('password')
+          .set(function(password)  {
               this._password = password;
               this.salt = uuidv1();
-              this.hashes_password = this.cryptPassword(password);
+              this.hashed_password = this.cryptPassword(password);
           })
-          .get(() => {return password})  ;
+          .get(function() {return this._password})  ;
 
 userScheme.methods = {
 
-    cryptPassword = function (password) {
+    cryptPassword : function(password) {
         if (!password) {
-            return '';
+            return 'h1';
         }
         try {
-           return crypto.createHmac('sha1', salt)
+           return crypto.createHmac('sha1', this.salt)
                   .update(password) 
                   .digest('hex') 
         }
         catch {
-            return '';
+            return 'h2';
         }
     }
 }  
